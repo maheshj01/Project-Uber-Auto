bd4217ef80ce508783bd92f75792e5d2aa6a78f5
maheshmnj <maheshmn121@gmail.com>
2018-09-13 10:07:20 +0530

    Validation to Register form

--- app/src/main/java/com/project/uberauto/RegisterActivity.java
+++ app/src/main/java/com/project/uberauto/RegisterActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -103,9 +104,86 @@ public class RegisterActivity extends AppCompatActivity {
                             // the auth state listener will be notified and logic to handle the
                             // signed in user can be handled in the listener.
                             if (!task.isSuccessful()) {
-                                Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException(),
-                                        Toast.LENGTH_LONG).show();
-                            } else {
+                                //        Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException(),
+                                //                Toast.LENGTH_LONG).show();
+                                String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+
+                                switch (errorCode) {
+
+                                    case "ERROR_INVALID_CUSTOM_TOKEN":
+                                        Toast.makeText(RegisterActivity.this, "The custom token format is incorrect. Please check the documentation.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_CUSTOM_TOKEN_MISMATCH":
+                                        Toast.makeText(RegisterActivity.this, "The custom token corresponds to a different audience.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_INVALID_CREDENTIAL":
+                                        Toast.makeText(RegisterActivity.this, "The supplied auth credential is malformed or has expired.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_INVALID_EMAIL":
+                                        Toast.makeText(RegisterActivity.this, "The email address is badly formatted.", Toast.LENGTH_LONG).show();
+                                        email.setError("The email address is badly formatted.");
+                                        email.requestFocus();
+                                        break;
+
+                                    case "ERROR_WRONG_PASSWORD":
+                                        Toast.makeText(RegisterActivity.this, "The password is invalid or the user does not have a password.", Toast.LENGTH_LONG).show();
+                                        passwd.setError("password is incorrect ");
+                                        passwd.requestFocus();
+                                        passwd.setText("");
+                                        break;
+
+                                    case "ERROR_USER_MISMATCH":
+                                        Toast.makeText(RegisterActivity.this, "The supplied credentials do not correspond to the previously signed in user.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_REQUIRES_RECENT_LOGIN":
+                                        Toast.makeText(RegisterActivity.this, "This operation is sensitive and requires recent authentication. Log in again before retrying this request.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
+                                        Toast.makeText(RegisterActivity.this, "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_EMAIL_ALREADY_IN_USE":
+                                       // Toast.makeText(RegisterActivity.this, "The email address is already in use by another account.   ", Toast.LENGTH_LONG).show();
+                                        email.setError("The email address is already in use.");
+                                        email.requestFocus();
+                                        break;
+
+                                    case "ERROR_CREDENTIAL_ALREADY_IN_USE":
+                                        Toast.makeText(RegisterActivity.this, "This credential is already associated with a different user account.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_USER_DISABLED":
+                                        Toast.makeText(RegisterActivity.this, "The user account has been disabled by an administrator.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_USER_TOKEN_EXPIRED":
+                                        Toast.makeText(RegisterActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_USER_NOT_FOUND":
+                                        Toast.makeText(RegisterActivity.this, "There is no user record corresponding to this identifier. The user may have been deleted.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_INVALID_USER_TOKEN":
+                                        Toast.makeText(RegisterActivity.this, "The user\\'s credential is no longer valid. The user must sign in again.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_OPERATION_NOT_ALLOWED":
+                                        Toast.makeText(RegisterActivity.this, "This operation is not allowed. You must enable this service in the console.", Toast.LENGTH_LONG).show();
+                                        break;
+
+                                    case "ERROR_WEAK_PASSWORD":
+                                      //  Toast.makeText(RegisterActivity.this, "The given password is invalid.", Toast.LENGTH_LONG).show();
+                                        passwd.setError("The password must be atleast 6 characters long");
+                                        passwd.requestFocus();
+                                        break;
+                                }
+                            }else { // register success
                                 pushdb(email.getText().toString().trim(),passwd.getText().toString().trim());
                                 startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                                 finish();
